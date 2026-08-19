e3dba9936b06b7476ff36baf9e9b6f3c526ed526
Mar-Jose <de.la.taes@gmail.com>
2026-08-19 17:30:42 -0300

    archivo profileHeader de presentación

---
+++ vite-project/src/components/ProfileHeader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ProfileHeader = () => {
+  return (
+    <div>
+        <h1> María José</h1>
+        <p> "...sapo de otro paso..."</p>
+        </div>
+  )
+}
+
+export default ProfileHeader
