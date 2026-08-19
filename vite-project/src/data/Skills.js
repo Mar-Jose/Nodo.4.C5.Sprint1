e996035a762ad13e1a297221f6efa96daf42c55f
Mar-Jose <de.la.taes@gmail.com>
2026-08-19 16:30:52 -0300

    creo carpetitas

---
+++ vite-project/src/data/Skills.js
@@ -0,0 +1,7 @@
+// src/data/skills.js
+export const skills = [
+  { id: 1, name: "HTML",       level: "Avanzado",    isFavorite: false },
+  { id: 2, name: "CSS",        level: "Intermedio",  isFavorite: false },
+  { id: 3, name: "JavaScript", level: "Intermedio",  isFavorite: true  },
+  { id: 4, name: "React",      level: "Aprendiendo", isFavorite: true  },
+];
